181e6f3835099690723bdc0383838497abe645f4
Joonas Sarajärvi <muep@iki.fi>
2024-10-31 21:08:14 +0100

    Day 1 just in Gambit r5rs

---
+++ run.scm
@@ -0,0 +1,21 @@
+(define (load-input path)
+  (let ((f (open-input-file path)))
+    (let loop ((buf '()))
+      (let ((line (read-line f)))
+        (if (eof-object? line)
+            (begin
+              (close-input-port f)
+              (reverse buf))
+            (loop (cons line buf)))))))
+
+(define (run-parts input parts n)
+  (unless (null? parts)
+    (display (string-append "  Part " (number->string n) ": "))
+    (display (number->string ((car parts) input)))
+    (newline)
+    (run-parts input (cdr parts) (+ n 1))))
+
+(display "Day 01\n")
+(load "day-01.scm")
+(define day-01-input (load-input "input/day-01.txt"))
+(run-parts day-01-input day-01 1)
