6fdc285a27ff8b409580426d6a7c0dee39114e2f
Joonas Sarajärvi <muep@iki.fi>
2024-11-08 22:43:55 +0100

    Revert "Day 1 just in Gambit r5rs"
    This reverts commit 181e6f3835099690723bdc0383838497abe645f4.

--- day-01.scm
+++ day-01.scm
@@ -1,30 +1,26 @@
-(define day-01
-  (let* ((foldl (lambda (f init seq-in)
-                  (let loop ((acc init)
-                             (seq seq-in))
-                    (if (null? seq)
-                        acc
-                        (loop (f (car seq) acc) (cdr seq))))))
-         (instruction->offset (lambda (instruction)
-                                (cond
-                                 ((equal? #\( instruction) 1)
-                                 ((equal? #\) instruction) -1)
-                                 (else 0))))
-         (part-1 (lambda (lines)
-                   (foldl + 0 (map instruction->offset
-                                   (string->list 
-                                    (apply string-append lines))))))
-         (part-2 (lambda (lines)
-                   (car
-                    (foldl
-                     (lambda (offset state)
-                       (let ((pos (car state))
-                             (sum (cdr state)))
-                         (if (negative? sum)
-                             state
-                             (cons (+ 1 pos) (+ sum offset)))))
-                     '(0 . 0)
-                     (map instruction->offset
-                          (string->list 
-                           (apply string-append lines))))))))
-    (list part-1 part-2)))
+#lang racket
+
+(define (slurp path)
+  (let ((port (open-input-file path)))
+    (port->string port #:close? #t)))
+
+(define (instruction->offset instruction)
+  (cond
+    ((equal? #\( instruction) 1)
+    ((equal? #\) instruction) -1)
+    (else 0)))
+
+(define (day-01-part-1 path)
+  (foldl + 0 (map instruction->offset (string->list (slurp path)))))
+
+(define (day-01-part-2 path)
+  (car
+   (foldl
+    (lambda (offset state)
+      (let ((pos (car state))
+            (sum (cdr state)))
+        (if (negative? sum)
+            state
+            (cons (+ 1 pos) (+ sum offset)))))
+    '(0 . 0)
+    (map instruction->offset (string->list (slurp path))))))
